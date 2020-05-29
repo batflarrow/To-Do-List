9079acb6f8eeb32d52855c85b1a2f84dec8ea736
batflarrow <56157504+batflarrow@users.noreply.github.com>
2020-05-29 19:29:22 +0530

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,53 +1,101 @@
 // JavaScript Document
+
+function liForList2(div){
+	var newli= document.createElement("li");
+	var value=div.innerHTML; 
+	newli.innerHTML=(value);
+	document.getElementById("list2").appendChild(newli);
+	
+}
+
+var myNodelist = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+var close = document.getElementsByClassName("close");
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+	div.style.display = "none";
+	if(div.parentElement.id=="list1")
+			{
+				liForList2(div);
+				
+			}
+  }
+}
+
+
+
+//var close = document.getElementsByClassName("close");
+//for (i = 0; i < close.length; i++) {
+//  close[i].onclick = function() {
+//    var div = this.parentElement;
+//	if(div.parentElement.id=="list1")
+//			{
+//	var newli= document.createElement("li");
+//	var value=div.innerHTML; 
+//	newli.innerHTML=(value);
+//	document.getElementById("list2").appendChild(newli);
+//			}
+//				div.style.display = "none";
+//  }
+//}
+
 var but=document.getElementById("button");
 
 but.onclick= function(){
-var value=document.getElementById("Input").value;
-if (value === '') {
-    alert("You must write something!");}
- else {
+	
 var newli= document.createElement("li");
+var value=document.getElementById("Input").value;
+
 newli.innerHTML=value;
-newli.setAttribute("title","Double Click On Me When Completed")
-newli.addEventListener("dblclick", addToSecondList);
-document.getElementById("list1").appendChild(newli);}
-document.getElementById("Input").value="";	
-}	
 
+if (value === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("list1").appendChild(newli);
+  }
+	document.getElementById("Input").value="";	
 
-function addToSecondList(event){
-	var secondList=document.getElementById("list2");
-	var firstList=document.getElementById("list1");
-	var newli= document.createElement("li");
-	newli.innerHTML = event.target.innerHTML;
-	newli.style.textDecoration='line-through';
-	newli.setAttribute("title","Double Click To remove and Click to send back to Undergoing task")
-	newli.addEventListener("dblclick", deleteFromSecondList);
-	newli.addEventListener("drag",backToFirstList)
-	secondList.appendChild(newli);
-	firstList.removeChild(event.target);
-	
+	var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  newli.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+		if(div.parentElement.id=="list1")
+			{
+				liForList2(div);
+			}
+		div.style.display = "none";
+    
+  }	
 }
-
-function backToFirstList(event){
-	var secondList=document.getElementById("list2");
-	var firstList=document.getElementById("list1");
-	var newli= document.createElement("li");
-	newli.innerHTML = event.target.innerHTML;
-	newli.addEventListener("dblclick", addToSecondList);
-	firstList.appendChild(newli);
-	firstList.appendChild(newli);
-	secondList.removeChild(event.target);
 	
 }
 
 
 
-function deleteFromSecondList(event) {
+//		
+//
+//		if(div.parentElement.id=="list1")
+//			{
+//		var newli= document.createElement("li");
+//	var value=div.innerHTML; 
+//	newli.innerHTML=(value);
+//	document.getElementById("list2").appendChild(newli);
+//				div.style.display = "none";
+//			}
+
+
+
 	
-	var secondList=document.getElementById("list2");
-	
-	secondList.removeChild(event.target);
-	
-	
-}
