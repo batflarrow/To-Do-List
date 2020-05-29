9e4e7aa099f9d45278c41a032a4b9abd91e2309a
batflarrow <56157504+batflarrow@users.noreply.github.com>
2020-05-29 19:06:04 +0530

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -9,7 +9,7 @@ if (value === '') {
 var newli= document.createElement("li");
 newli.innerHTML=value;
 	 newli.setAttribute("title","Double Click On Me When Completed")
-newli.addEventListener("dblclick", addToSecondList);
+newli.addEventListener("click", addToSecondList);
 document.getElementById("list1").appendChild(newli);}
 document.getElementById("Input").value="";	
 }	
@@ -22,7 +22,7 @@ function addToSecondList(event){
 	newli.innerHTML = event.target.innerHTML;
 	newli.style.textDecoration='line-through';
 	newli.setAttribute("title","Double Click To remove and Click to send back to Undergoing task")
-	newli.addEventListener("dblclick", deleteFromSecondList);
+	newli.addEventListener("click", deleteFromSecondList);
 	newli.addEventListener("drag",backToFirstList)
 	secondList.appendChild(newli);
 	firstList.removeChild(event.target);
